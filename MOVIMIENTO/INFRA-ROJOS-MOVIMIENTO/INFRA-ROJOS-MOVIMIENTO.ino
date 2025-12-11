71a6bae3da56c391197a773dd7f867ad00f396d1
luisitoon28 <jl.laria.2023@alumnos.urjc.es>
2025-12-11 14:24:29 +0100

    Inicio pid

--- MOVIMIENTO/INFRA-ROJOS-MOVIMIENTO/INFRA-ROJOS-MOVIMIENTO.ino
+++ MOVIMIENTO/INFRA-ROJOS-MOVIMIENTO/INFRA-ROJOS-MOVIMIENTO.ino
@@ -1,93 +1,126 @@
-#include <Thread.h>
-#include <ThreadController.h>
-
+// --- Configuración de Pines Sensores ---
 #define PIN_ITR20001_LEFT   A2
 #define PIN_ITR20001_MIDDLE A1
 #define PIN_ITR20001_RIGHT  A0
 
-// Máquina de estados del motor
-enum movimiento {
-  LINEA_ENCONTRADA,           
-  BUSCANDO_LINEA            
+// --- Definición de Pines Motores ---
+#define PIN_Motor_STBY  3
+// Grupo A (Derecha)
+#define PIN_Motor_AIN_1 7
+#define PIN_Motor_PWMA  5
+// Grupo B (Izquierda)
+#define PIN_Motor_BIN_1 8
+#define PIN_Motor_PWMB  6 
+
+float kp = 0.001;
+float kd = 2;
+
+int error;
+int speed_left;
+int speed_right;
+
+int derivate;
+int prev_error;
+
+int velocidadBase = 60;
+
+// Enum para ver cual es el útimo infrarojos en detectar linea negra
+enum LastInfra {
+  LEFT,
+  MIDDLE,
+  RIGHT,
 };
-movimiento estadoActual = LINEA_ENCONTRADA; 
 
-// 1. Instanciamos el Controlador (opcional, pero recomendado para manejar muchos hilos)
-ThreadController controlador = ThreadController();
+int maxspeed = 255;
+int vals[3];
+int speeds[2];
 
-// 2. Creamos los Hilos
-Thread hilo_infra_rojos = Thread();
-Thread hilo_motor = Thread();
+// Devuelve un puntero al array de lecturas
+void read_infra()
+{
+  int valLeft = analogRead(PIN_ITR20001_LEFT);
+  int valMiddle = analogRead(PIN_ITR20001_MIDDLE);
+  int valRight = analogRead(PIN_ITR20001_RIGHT);
 
-int valLeft = analogRead(PIN_ITR20001_LEFT);
-int valMiddle = analogRead(PIN_ITR20001_MIDDLE);
-int valRight = analogRead(PIN_ITR20001_RIGHT);
+  // Debugging para Serial Plotter
+  Serial.print("Izquierda:"); Serial.print(valLeft); Serial.print(", ");
+  Serial.print("Centro:"); Serial.print(valMiddle); Serial.print(", ");
+  Serial.print("Derecha:"); Serial.println(valRight);
 
-void callback_infrarojo() {
-  valLeft = analogRead(PIN_ITR20001_LEFT);
-  valMiddle = analogRead(PIN_ITR20001_MIDDLE);
-  valRight = analogRead(PIN_ITR20001_RIGHT);
-  if (PIN_ITR20001_LEFT < 100 && PIN_ITR20001_RIGHT< 100){
-    estadoActual = BUSCANDO_LINEA;
-  }else{
-    estadoActual = LINEA_ENCONTRADA;
-  }
+  vals[0] = valLeft;
+  vals[1] = valMiddle;
+  vals[2] = valRight;
 }
 
-bool motoor = false;
-void callback_motor() {
-  motoor = true;
-}
+void pid(int* inf_vals)
+{
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
+  int left = inf_vals[0];
+  int middle = inf_vals[1];
+  int right = inf_vals[2];
 
-  // 4. Configuramos el Hilo del LED
-  hilo_infra_rojos.onRun(callback_infrarojo);    // Qué función ejecutar
-  hilo_infra_rojos.setInterval(50);      // Cada cuánto (500ms)
-
-  // 5. Configuramos el Hilo del Serial
-  hilo_motor.onRun(callback_motor);
-  hilo_motor.setInterval(50);  // Cada cuánto (1000ms)
-
-  // 6. Añadimos los hilos al controlador
-  controlador.add(&hilo_infra_rojos);
-  controlador.add(&hilo_motor);
-}
-
-void loop() {
-  // 7. Ejecutamos el controlador (revisa todos los hilos automáticamente)
-  controlador.run();
+  error = right - left;
   
-  if (motoor == true){
-    if (movimiento == LINEA_ENCONTRADA) {
+  derivate = error - prev_error;
+  prev_error = error;
 
-  } else if (movimiento == BUSCANDO_LINEA){
+  speed_left = velocidadBase + kp * error + kd * derivate;
+  speed_right = velocidadBase - (kp * error + kd * derivate);
 
+  speed_left = constrain(speed_left, 0, 255);
+  speed_right = constrain(speed_right, 0, 255);
+
+  speeds[0] = speed_left;
+  speeds[1] = speed_right;
+
+  if (left < 300 && middle < 300 && right < 300) {
+    speeds[0] = 0;
+    speeds[1] = 0;
   }
+
+  Serial.print("Left: ");
+  Serial.print(speeds[0]);
+  Serial.print(" ");
+  Serial.print("Right: ");
+  Serial.print(speeds[1]);
+  Serial.print(" ");
 }
 
+void move(int* speeds)
+{
+  speed_left = speeds[0];
+  speed_right = speeds[1];
 
+  digitalWrite(PIN_Motor_BIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMB, speed_left);
 
+  digitalWrite(PIN_Motor_AIN_1, HIGH);
+  analogWrite(PIN_Motor_PWMA, speed_right);
+}
 
+void setup()
+{
+  Serial.begin(9600);
 
+  // Configuración de pines como Salida
+  pinMode(PIN_Motor_STBY, OUTPUT);
+  pinMode(PIN_Motor_AIN_1, OUTPUT);
+  pinMode(PIN_Motor_PWMA, OUTPUT);
+  pinMode(PIN_Motor_BIN_1, OUTPUT);
+  pinMode(PIN_Motor_PWMB, OUTPUT);
 
+  // Habilitar la controladora de motores
+  digitalWrite(PIN_Motor_STBY, HIGH);
+}
 
+void loop()
+{
+   // 1. Leer sensores
+  read_infra();
+  
+  // 2. Calcular PID
+  pid(vals);
 
+  //move(speeds);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
